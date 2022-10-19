0d4b6e6cb7bf939759cd20aaf90e87ff1b36d727
manojap <manojap@outlook.com>
2022-10-19 23:32:29 +0530

    🎨 IMPROVE: .

--- components/layout.js
+++ components/layout.js
@@ -4,13 +4,13 @@ import Meta from '../components/meta'
 
 export default function Layout({ preview, children }) {
   return (
-    <>
+    <div className="bg-slate-800 text-white">
       <Meta />
       <div className="min-h-screen">
         <Alert preview={preview} />
         <main>{children}</main>
       </div>
       <Footer />
-    </>
+    </div>
   )
 }
